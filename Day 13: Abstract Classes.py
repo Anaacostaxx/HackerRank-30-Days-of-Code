a5babea11a2c144153e8daae35549d855e49d4ee
Ana Acosta <ana.armstrong.dirnt@gmail.com>
2021-10-01 13:19:57 -0500

    Create Day 13: Abstract Classes.py
    Given a Book class and a Solution class, write a MyBook class that does the following:
    
    Inherits from Book
    Has a parameterized constructor taking these  parameters:
    string 
    string 
    int 
    Implements the Book class' abstract display() method so it prints these  lines:
    , a space, and then the current instance's .
    , a space, and then the current instance's .
    , a space, and then the current instance's .

---
+++ Day 13: Abstract Classes.py
@@ -0,0 +1,17 @@
+class Book(object, metaclass=ABCMeta):
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    @abstractmethod
+    def display(self): pass
+
+
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        super().__init__(title, author)
+        self.price = price
+
+    def display(self):
+        print("Title: " + self.title + "\nAuthor: " + self.author + "\nPrice: " + str(self.price))
+
